0f722af9d35e29cf21fcbdfc00e72119c5318cae
Philip Pincus <philippincus@Philips-MacBook-Pro.local>
2022-10-22 14:05:53 +0100

    initial commit to create test set and income categories hist

--- housing.py
+++ housing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
+# In[4]:
 
 
 # import data
@@ -25,7 +25,7 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
 fetch_housing_data()
 
 
-# In[7]:
+# In[5]:
 
 
 # load data
@@ -37,7 +37,7 @@ def load_housing_data(housing_path=HOUSING_PATH):
     return pd.read_csv(csv_path)
 
 
-# In[9]:
+# In[6]:
 
 
 # eda
@@ -45,7 +45,7 @@ def load_housing_data(housing_path=HOUSING_PATH):
 housing = load_housing_data()
 
 
-# In[13]:
+# In[1]:
 
 
 # housing.head()
@@ -53,7 +53,64 @@ housing = load_housing_data()
 # housing["ocean_proximity"].value_counts()
 # housing.describe()
 
-import matplotlib.pyplot as plt
-housing.hist(bins=50, figsize=(20,15))
-plt.show()
+# import matplotlib.pyplot as plt
+# housing.hist(bins=50, figsize=(20,15))
+# plt.show()
+
+
+# In[7]:
+
+
+# create test data
+
+import numpy as np
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+# In[9]:
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+
+# len(train_set)
+# len(test_set)
+
+
+# In[14]:
+
+
+# avoid duplication of test set
+
+from zlib import crc32
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+# create identifier column
+
+housing_with_id = housing.reset_index() # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+
+
+# In[15]:
+
+
+# income categories
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                                labels=[1, 2, 3, 4, 5])
+
+housing["income_cat"].hist()
 
